23a320bb13f613a2cb186bfa4e256c7e65c6a9ec
GelfandKonstantin <k.ermakov34@gmail.com>
2019-01-25 10:14:50 +0200

    1.3. JavaDoc. Code convention[#103062]

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Konstantin Gelfand (k.ermakov34@gmail.com)
+ 
 public class Calculate {
-	
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Konstantin Gelfand (k.ermakov34@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
