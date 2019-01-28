c067e6710bbf870a2cd6ebeccb51dc262db25af6
GelfandKonstantin <k.ermakov34@gmail.com>
2019-01-28 09:46:46 +0200

    1.3.1 Maven Plugins[#103059]

--- chapter_001/src/main/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/ru/job4j/calculate/Calculate.java
@@ -4,8 +4,8 @@ package ru.job4j.calculate;
  * Calculate.
  *
  * @author Konstantin Gelfand (k.ermakov34@gmail.com)
- 
-public class Calculate {
+ */
+ public class Calculate {
 	/**
 	* Конструктор, вывод строки в консоль.
 	* @param arg - arg.
