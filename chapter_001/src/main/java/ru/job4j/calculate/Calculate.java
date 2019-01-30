e3dc40e96cfbf66ce34f831de9eb2f7cdae2407a
GelfandKonstantin <k.ermakov34@gmail.com>
2019-01-30 14:33:16 +0200

    1.4. JUnit[#103063]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,15 +4,24 @@ package ru.job4j.calculate;
  * Calculate.
  *
  * @author Konstantin Gelfand (k.ermakov34@gmail.com)
+ * @version $Id$
+ * @since 0.1
  */
  public class Calculate {
 	/**
-	* Конструктор, вывод строки в консоль.
+	* Main.
 	* @param arg - arg.
 	*/
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
 	}
-
+    /**
+	 *Method echo.
+	 *@param name Konstantin.
+	 *@return Echo plus your name.
+	 */
+	public String echo(String name) {
+         return "Echo, echo, echo : " + name;
+    }	
 }
 	
